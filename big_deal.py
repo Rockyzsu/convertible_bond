70cf5a1872f28970c652131590600d3d0604b8b3
Rocky Chen <jinweizsu@gmail.com>
2019-04-04 17:58:12 +0800

    得到获取结果

--- big_deal.py
+++ big_deal.py
@@ -1,26 +1,148 @@
 # 监控可转债大单
 import datetime
+import json
+import logging
+import os
+import random
+import time
 
+import pymongo
 import tushare as ts
 import pandas as pd
+from setting import get_engine
 
 
 class BigDeal(object):
 
     def __init__(self):
-        # self.df = self.get_tick()
-        pass
 
-    def get_tick(self,code,date):
-        df=ts.get_tick_data(code,date=date,src='tt')
+        # self.df = self.get_tick()
+        self.logger = self.llogger('fetch_daily')
+        self.db_stock_engine = get_engine('db_stock', True)
+        self.jisilu_df = self.get_code()
+        self.db = pymongo.MongoClient('10.18.6.46', 27001)
+
+    def llogger(self, filename):
+        logger = logging.getLogger(filename)  # 不加名称设置root logger
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            '[%(asctime)s][%(filename)s][line: %(lineno)d]\[%(levelname)s] ## %(message)s)',
+            datefmt='%Y-%m-%d %H:%M:%S')
+        # 使用FileHandler输出到文件
+        prefix = os.path.splitext(filename)[0]
+        fh = logging.FileHandler(prefix + '.log')
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        # 使用StreamHandler输出到屏幕
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        # 添加两个Handler
+        logger.addHandler(ch)
+        logger.addHandler(fh)
+        # logger.info('this is info message')
+        # logger.warning('this is warn message')
+        return logger
+
+    def get_ticks(self, code, date):
+        df = ts.get_tick_data(code, date=date, src='tt')
         # today = datetime.date.today().strftime('%Y-%m-%d')
-        df['time']=df['time'].map(lambda x:date+' '+x)
+        df['time'] = df['time'].map(lambda x: date + ' ' + x)
         return df
 
+    # 从mongo获取数据
     def analysis(self):
-        df = self.get_tick()
-        df['time']=pd.to_datetime(df['time'],format='%Y-%m-%d %H:%M:%S')
-        df = df.set_index('time',drop=True)
+        code='110030'
+        doc = self.db['cb_deal'][code]
+        d=[]
+        for item in doc.find({},{'_id':0}):
+            d.append(item)
+        df = pd.DataFrame(d)
+        print(df.info())
+        input()
 
-    def loop_date(self):
-        
+
+    def get_tick(self, code, date):
+        fs_df = None
+
+        for i in range(10):
+
+            try:
+                fs_df = ts.get_tick_data(code, date=date, src='tt')
+
+            except Exception as e:
+                self.logger.error('获取tick失败>>>>code={},date'.format(code, date))
+                self.logger.error(e)
+                time.sleep(random.randint(2, 5))
+
+            else:
+                if fs_df is not None and len(fs_df) > 0:
+                    break
+                else:
+                    self.logger.error('>>>>code={},date={} 获取行情重试 {}次'.format(code, date, i))
+
+        return fs_df
+
+    def loop_code(self, date):
+
+        code_list = self.jisilu_df['可转债代码'].values
+
+        for code in code_list:
+            fs_df = self.get_tick(code, date)
+
+            if fs_df is None:
+                continue
+
+            fs_df['time'] = fs_df['time'].map(lambda x: date + ' ' + x)
+            fs_df['time'] = pd.to_datetime(fs_df['time'], format='%Y-%m-%d %H:%M:%S')
+
+            ret = self.store(code, fs_df)
+
+            if ret.get('status') == -1:
+                self.logger.error('保存失败 >>>> code={}, date={}'.format(code, date))
+
+    def store(self, code, df):
+        df['code'] = code
+        js = json.loads(df.T.to_json()).values()
+        for row in js:
+            row['time'] = datetime.datetime.utcfromtimestamp(row['time'] / 1000)
+        try:
+            self.db['cb_deal'][code].insert_many(js)
+        except Exception as e:
+            print(e)
+            print('插入数据失败')
+            return {'status': -1, 'code': code}
+        else:
+            return {'status': 0, 'code': code}
+
+    def get_code(self):
+        df = pd.read_sql('tb_bond_jisilu', con=self.db_stock_engine)
+        return df
+
+    def loop_date(self,today=True):
+
+        # 获取当天数据，18点之后
+        if today:
+            d = datetime.date.today().strftime('%Y-%m-%d')
+            if ts.is_holiday(d):
+                print('holiday,skip>>>>{}'.format(d))
+            else:
+                print('going>>>>{}'.format(d))
+                self.loop_code(d)
+        # 获取一周的数据看看
+
+        else:
+            delta = 7
+            for i in range(1, delta + 1):
+                d = (datetime.date.today() + datetime.timedelta(days=i * -1)).strftime('%Y-%m-%d')
+                if ts.is_holiday(d):
+                    print('holiday,skip>>>>{}'.format(d))
+                else:
+                    print('going>>>>{}'.format(d))
+                    self.loop_code(d)
+
+
+if __name__ == '__main__':
+    obj = BigDeal()
+    # obj.loop_date()
+    obj.analysis()
---
+++ task_cb_deal.py
@@ -0,0 +1,7 @@
+# -*-coding=utf-8-*-
+
+# @Time : 2019/4/4 17:08
+# @File : task_cb_deal.py
+from big_deal import BigDeal
+obj = BigDeal()
+obj.loop_date()
