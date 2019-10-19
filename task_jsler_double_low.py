90daae948d354a7d886331805878106a0e8ecb25
Rocky Chen <chenjinwei@beyebe.com>
2019-10-19 16:23:37 +0800

    update

--- task_jsler_double_low.py
+++ task_jsler_double_low.py
@@ -5,31 +5,47 @@
 from settings import get_engine
 import pandas as pd
 import datetime
-from settings import sender_139_jsl,
+from settings import jsl_mail_sender, llogger, send_aliyun, QQ_MAIL
 
-def calculation(x):
+logger = llogger('log/task_jsler_double_low.log')
 
-    return x
+today = datetime.datetime.now().strftime('%Y-%m-%d')
+
 
 def cb_info():
-    con = get_engine('db_stock','local')
-    df = pd.read_sql('tb_bond_jisilu',con=con)
+    con = get_engine('db_stock', 'local')
+    df = pd.read_sql('tb_bond_jisilu', con=con)
 
     df['可转债综合价格'] = df['可转债价格'] + df['溢价率']
     df = df.sort_values(by='可转债综合价格')
-    df1=df[['可转债代码','可转债名称','可转债综合价格','可转债价格','溢价率']].head(10)
-    df1=df1.reset_index(drop=True)
-    send_content=df1.to_html()
+    df1 = df[['可转债代码', '可转债名称', '可转债综合价格', '可转债价格', '溢价率']].head(20)
+    df1 = df1.reset_index(drop=True)
+    send_content = df1.to_html()
     # send_content=send_content+'\n\n默认每周一开盘前发送一次，如果有其他需求请回复。\n'
-    send_content=send_content
-    title = '{}可转债综合价格前10名'.format(datetime.datetime.now().strftime('%Y-%m-%d'))
+    # send_content = send_content
+    title = '{} 可转债综合价格前10名'.format(datetime.datetime.now().strftime('%Y-%m-%d'))
+    conn2 = get_engine('double_low_full')
+    df1.to_sql(f'double_low_{today}',con=conn2)
 
     try:
-        sender_139_jsl(title,send_content,types='html')
+        send_aliyun(title, send_content, QQ_MAIL)
+        # print(QQ_MAIL)
+        # jsl_mail_sender(title,send_content,types='html')
     except Exception as e:
         print('报错了')
 
 
-if __name__=='__main__':
-    cb_info()
+if __name__ == '__main__':
+    import tushare as ts
 
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    cons = ts.get_apis()
+    if ts.is_holiday(today):
+        ts.close_apis(cons)
+        exit()
+
+    else:
+        # TODO
+        cb_info()
+
+        ts.close_apis(cons)
