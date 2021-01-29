dd9b46b33d30a645664288ee4101b1698a2cbb35
Rocky Chen <weigesysu@qq.com>
2021-01-29 17:19:06 +0800

    local update

--- datahub/cb_index.py
+++ datahub/cb_index.py
@@ -4,16 +4,17 @@
 # @File : cb_index.py
 # 可转债指数
 import re
+import sys
+sys.path.append('..')
 import requests
 from configure.settings import DBSelector
 from common.BaseService import BaseService
 
-DB = DBSelector()
-
 # 获取当天的记录
 class CBIndexJSL(BaseService):
     def __init__(self):
-        super(CBIndexJSL, self).__init__('log/cb_index.log')
+        super(CBIndexJSL, self).__init__('../log/cb_index.log')
+        self.db = DBSelector()
 
     def get_today_index(self):
         url = 'https://www.jisilu.cn/data/cbnew/cb_index_quote/'
@@ -42,7 +43,7 @@ class CBIndexJSL(BaseService):
         update_data_sql = '''
         insert into `tb_cb_index` (`日期`,`指数`,`成交额(亿元)`,`涨跌`,`涨跌额`,`转债数目`,`剩余规模`) values (%s,%s,%s,%s,%s,%s,%s);
         '''
-        con = DB.get_mysql_conn('db_stock', 'qq')
+        con = self.db.get_mysql_conn('db_stock', 'qq')
         self.execute(update_data_sql, data, con)
 
     # 获取历史记录
@@ -69,15 +70,15 @@ class CBIndexJSL(BaseService):
         increase_val = data_.get('increase_val')  # 涨跌额
         increase_rt = data_.get('increase_rt')  # 涨跌幅
 
-        con = DB.get_mysql_conn('db_stock', 'qq')
+        con = self.db.get_mysql_conn('db_stock', 'qq')
 
         create_sql = '''
-        create table if not exists tb_cb_index (`日期` date ,`指数` float ,`成交额(亿元)` float,`涨跌` float ,`涨跌额` float ,`转债数目` float ,`剩余规模` float )
+        create table if not exists tb_cb_index_test (`日期` date ,`指数` float ,`成交额(亿元)` float,`涨跌` float ,`涨跌额` float ,`转债数目` float ,`剩余规模` float )
         '''
         self.execute(create_sql, (), con)
 
         update_data_sql = '''
-        insert into `tb_cb_index` (`日期`,`指数`,`成交额(亿元)`,`涨跌`,`涨跌额`,`转债数目`,`剩余规模`) values (%s,%s,%s,%s,%s,%s,%s);
+        insert into `tb_cb_index_test` (`日期`,`指数`,`成交额(亿元)`,`涨跌`,`涨跌额`,`转债数目`,`剩余规模`) values (%s,%s,%s,%s,%s,%s,%s);
         '''
         for index, item in enumerate(date_):
             value_tuple = (
@@ -87,11 +88,11 @@ class CBIndexJSL(BaseService):
 
 
 def main():
-    # history_data()  # 第一次获取历史数据， 后面获取每一天的数据
-    # get_today_index()
     app = CBIndexJSL()
     app.get_today_index()
 
+    # 获取历史数据 用于分析
+    # app.history_data()
 
 if __name__ == '__main__':
     main()
