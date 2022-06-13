d51e180460b71c49c184c8b3d7d1cb238eab0a80
Rockyzsu <jinweizsu@gmail.com>
2022-06-13 23:59:15 +0800

    基金持仓

--- datahub/cb_index.py
+++ datahub/cb_index.py
@@ -15,6 +15,8 @@ class CBIndexJSL(BaseService):
     def __init__(self):
         super(CBIndexJSL, self).__init__('../log/cb_index.log')
         self.db = DBSelector()
+        self.to_mysql = False
+
 
     def get_today_index(self):
         url = 'https://www.jisilu.cn/data/cbnew/cb_index_quote/'
@@ -48,6 +50,8 @@ class CBIndexJSL(BaseService):
 
     # 获取历史记录
     def history_data(self):
+        import demjson
+
         url = 'https://www.jisilu.cn/data/cbnew/cb_index/'
         headers = {'Host': 'www.jisilu.cn',
                    'Referer': 'https://www.jisilu.cn/data/cbnew/',
@@ -61,38 +65,50 @@ class CBIndexJSL(BaseService):
         content = r.text
         date = re.search('var __date = (.*?);', content, re.S).group(1)
         data = re.search('var __data = (.*?);', content, re.S).group(1)
-        date_ = eval(date)
-        data_ = eval(data)
-        price = data_.get('price')
-        volume = data_.get('volume')
-        amount = data_.get('amount')
-        count = data_.get('count')  # 数量
-        increase_val = data_.get('increase_val')  # 涨跌额
-        increase_rt = data_.get('increase_rt')  # 涨跌幅
+        data_py = demjson.decode(data)
+        date_py = demjson.decode(date)
+        # print(data_py.get('price'))
 
-        con = self.db.get_mysql_conn('db_stock', 'qq')
+        # date_ = eval(date)
+        # data_ = eval(data)
+        # price = data_py.get('price')
+        # volume = data_py.get('volume')
+        # amount = data_.get('amount')
+        # count = data_.get('count')  # 数量
+        # increase_val = data_.get('increase_val')  # 涨跌额
+        # increase_rt = data_.get('increase_rt')  # 涨跌幅
+        if self.to_mysql:
 
-        create_sql = '''
-        create table if not exists tb_cb_index_test (`日期` date ,`指数` float ,`成交额(亿元)` float,`涨跌` float ,`涨跌额` float ,`转债数目` float ,`剩余规模` float )
-        '''
-        self.execute(create_sql, (), con)
+            con = self.db.get_mysql_conn('db_stock', 'qq')
 
-        update_data_sql = '''
-        insert into `tb_cb_index_test` (`日期`,`指数`,`成交额(亿元)`,`涨跌`,`涨跌额`,`转债数目`,`剩余规模`) values (%s,%s,%s,%s,%s,%s,%s);
-        '''
-        for index, item in enumerate(date_):
-            value_tuple = (
-                item, price[index], volume[index], increase_val[index], increase_rt[index], count[index], amount[index])
+            create_sql = '''
+            create table if not exists tb_cb_index_test (`日期` date ,`指数` float ,`成交额(亿元)` float,`涨跌` float ,`涨跌额` float ,`转债数目` float ,`剩余规模` float )
+            '''
+            self.execute(create_sql, (), con)
 
-            self.execute(update_data_sql, value_tuple, con)
+            update_data_sql = '''
+            insert into `tb_cb_index_test` (`日期`,`指数`,`成交额(亿元)`,`涨跌`,`涨跌额`,`转债数目`,`剩余规模`) values (%s,%s,%s,%s,%s,%s,%s);
+            '''
+            for index, item in enumerate(date_):
+                value_tuple = (
+                    item, price[index], volume[index], increase_val[index], increase_rt[index], count[index], amount[index])
+
+                self.execute(update_data_sql, value_tuple, con)
+
+        else:
+            # 存excel
+            import pandas as pd
+
+            df = pd.DataFrame({'date':date_py,'index':data_py.get('price')})
+            df.to_excel('../data/jsl_index.xlsx',encoding='utf8')
 
 
 def main():
     app = CBIndexJSL()
-    app.get_today_index()
+    # app.get_today_index()
 
     # 获取历史数据 用于分析
-    # app.history_data()
+    app.history_data()
 
 if __name__ == '__main__':
     main()
