77d71feb16144679ae59611a7a1d0fd09362e1d5
Rockyzsu <jinweizsu@gmail.com>
2022-08-09 22:50:16 +0800

    绘制曲线

---
+++ datahub/bond_zg_ripple_parallel.py
@@ -0,0 +1,45 @@
+# 转债正股 波动率计算
+import datetime
+import akshare as ak
+import pandas as pd
+import requests
+
+sign = '01A5817AF079690D5F80B63499BDF49F' # 需要的话私信星主
+HOST='117.50.175.2'
+PORT='23130'
+
+def get_bond_basic_info():
+    host = 'http://{}:{}/{}'
+    atction = 'info'
+    data = {'sign': sign, 'args': None}
+    resp = requests.post(
+        url=host.format(HOST,PORT,atction), data=data,
+   )
+
+    ret_data = resp.json()['data']
+    df = pd.DataFrame(ret_data)
+    return df
+
+def last_n_day(date, n, origin_fmt, target_fmt):
+    return (datetime.datetime.strptime(date, origin_fmt) - datetime.timedelta(days=n)).strftime(target_fmt)
+
+def main():
+    fmt = '%Y%m%d'
+    jsl_bond_df=get_bond_basic_info()
+
+    zg_code = jsl_bond_df['zg_code'].tolist()
+    today=datetime.date.today().strftime(fmt)
+    start_date=last_n_day(today,30,fmt,fmt)
+    iv_dict={}
+
+    for code in zg_code:
+        print('正在获取代码....{}'.format(code))
+        stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=code, period="daily", start_date=start_date, end_date=today, adjust="qfq")
+        iv = stock_zh_a_hist_df['涨跌幅'].std()
+        iv_dict[code]=iv
+
+    jsl_bond_df['iv']=jsl_bond_df['zg_code'].map(lambda x:iv_dict.get(x))
+    jsl_bond_df.to_excel('{}正股波动率.xlsx'.format(today),encoding='utf8')
+
+if __name__ == '__main__':
+    main()
